9f8116bf4b248d3d4a8b07ff5d2ae05b3db7d475
CzerwonyWiewior <mateuszkuzniarek@gmail.com>
2018-04-05 07:24:50 +0200

    Dodano mozliwosc wczytywania przykladow z pliku

--- Analiza1/src/perceptron/Perceptron.java
+++ Analiza1/src/perceptron/Perceptron.java
@@ -1,11 +1,14 @@
 package perceptron;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Perceptron 
 {
 	private ArrayList<Double> initialInputs = new ArrayList<Double>();
 	private ArrayList<Layer> layers = new ArrayList<Layer>();
+	private ArrayList<TrainingExample> examples = new ArrayList<TrainingExample>();
 	
 	public void initializePerceptron() throws Exception
 	{
@@ -31,6 +34,33 @@ public class Perceptron
 			inputs = layers.get(i).getOutputs(); 
 		}
 		return inputs;
+	}
+	
+	public void getExamplesFromFile(String path, int inputs, int outputs) throws Exception
+	{
+		File file = new File(path);
+		try
+		{
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNext()) 
+			{
+				TrainingExample example = new TrainingExample();
+				for(int i=0; i<inputs; i++)
+				{
+					example.getInputs().add(Double.valueOf(scanner.next()));
+				}
+				for(int i=0; i<outputs; i++)
+				{
+					example.getInputs().add(Double.valueOf(scanner.next()));
+				}
+				examples.add(example);
+			}
+			scanner.close();
+		}
+		catch(Exception e)
+		{
+			throw new Exception();
+		}
 	}
 
 }
---
+++ Analiza1/src/perceptron/TrainingExample.java
@@ -0,0 +1,19 @@
+package perceptron;
+
+import java.util.ArrayList;
+
+public class TrainingExample 
+{
+	private ArrayList<Double> inputs = new ArrayList<Double>();
+	private ArrayList<Double> outputs = new ArrayList<Double>();
+	
+	public ArrayList<Double> getInputs()
+	{
+		return inputs;
+	}
+	
+	public ArrayList<Double> getOutputs()
+	{
+		return outputs;
+	}
+}
