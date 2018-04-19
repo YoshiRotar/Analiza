ac04f562a8ec793970a7a3dda2290369d7aad2c6
CzerwonyWiewior <mateuszkuzniarek@gmail.com>
2018-04-19 04:00:54 +0200

    Dane do 4b

--- Analiza1/src/perceptron/Main.java
+++ Analiza1/src/perceptron/Main.java
@@ -8,41 +8,41 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		String path;
-		for(int j=4;j<=16;j+=4)
-		{
+		//for(int j=4;j<=16;j+=4)
+		//{
 			for(int i=1;i<=5;i++)
 			{
 				Layer layer1 = new Layer();
-				layer1.fillWithLogisticNeurons(j);
+				layer1.fillWithLogisticNeurons(4);
 				
 				Layer layer2 = new Layer();
-				layer2.fillWithLogisticNeurons(3);
+				layer2.fillWithLinearNeurons(1);
 				
 				Perceptron p = new Perceptron();
 				p.addLayer(layer1);
 				p.addLayer(layer2);
 				
 				//p.setBias(false);
-				p.setMomentum(0.1);
-				p.setRateOfChange(0.1);
+				p.setMomentum(0.5);
+				p.setRateOfChange(0.5);
 				
 				try 
 				{
-					p.getTrainingExamplesFromFile("./data/classification_train.txt", 4, 1);
-					p.getTestExamplesFromFile("./data/classification_test.txt", 4, 1);	
-					p.formatExampleOutputs();
-					path = "./data/5/24/n"+j+"/przebieg"+i+".csv";
+					p.getTrainingExamplesFromFile("./data/approximation_train_2.txt", 1, 1);
+					p.getTestExamplesFromFile("./data/approximation_test.txt", 1, 1);	
+					//p.formatExampleOutputs();
+					path = "./data/4/wplywParametrow/r05m05/przebieg"+i+".csv";
 					p.setLogPath(path);
-					p.ignoreInputs(new ArrayList<Integer>(Arrays.asList(1,3)));
+					//p.ignoreInputs(new ArrayList<Integer>(Arrays.asList(1,3)));
 					p.initializePerceptron();
 					p.learn(2500);
 					
 					p.resetExamples();
-					p.getTrainingExamplesFromFile("./data/classification_train.txt", 4, 1);
-					p.getTestExamplesFromFile("./data/classification_test.txt", 4, 1);	
-					p.formatExampleOutputs();
-					path = "./data/5/24/n"+j+"/wyniki"+i+".csv";
-					p.ignoreInputs(new ArrayList<Integer>(Arrays.asList(1,3)));
+					p.getTrainingExamplesFromFile("./data/approximation_train_2.txt", 1, 1);
+					p.getTestExamplesFromFile("./data/approximation_test.txt", 1, 1);	
+					//p.formatExampleOutputs();
+					path = "./data/4/wplywParametrow/r05m05/wyniki"+i+".csv";
+					//p.ignoreInputs(new ArrayList<Integer>(Arrays.asList(1,3)));
 					p.setLogPath(path);
 					p.sendOutputToLog();
 				} 
@@ -51,7 +51,7 @@ public class Main {
 					e.printStackTrace();
 				}
 			}
-		}
+		//}
 		System.out.println("koniec");
 	}
 
